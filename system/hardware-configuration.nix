3e01e34d52dd4502a6f37d98c8575f28e18d8ee4
Brandon Ogle <brandon@standardcognition.com>
2021-01-25 15:57:34 -0600

    Syntax tidying of an import statement in hardware-config

--- system/hardware-configuration.nix
+++ system/hardware-configuration.nix
@@ -4,9 +4,7 @@
 { config, lib, pkgs, ... }:
 
 {
-  imports =
-    [ <nixpkgs/nixos/modules/installer/scan/not-detected.nix>
-    ];
+  imports = [ <nixpkgs/nixos/modules/installer/scan/not-detected.nix> ];
 
   boot.initrd.availableKernelModules = [ "xhci_pci" "nvme" "usb_storage" "sd_mod" ];
   boot.kernelModules = [ "kvm-intel" ];
