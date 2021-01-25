7880b4555c589cec60d09d54d6850a21e28cf607
Brandon Ogle <brandon@standardcognition.com>
2021-01-25 16:45:43 -0600

    Setup desktop environment: display manager, audio, bluetooth, etc

---
+++ system/desktop.nix
@@ -0,0 +1,46 @@
+{ config, pkgs, ... }:
+
+{
+
+  # Enable the X11 windowing system.
+  services.xserver = {
+    displayManager.autoLogin.enable = true;
+    displayManager.lightdm.enable = true;
+    videoDrivers = ["intel" "nvidia" "modesetting"];
+    xkbOptions = "caps:escape";
+    enable = true;
+  };
+
+  # Enable touchpad support.
+  services.xserver.libinput = {
+    enable = true;
+    tapping = false;
+  };
+
+  # Enable audio
+  sound.enable = true;
+  hardware.pulseaudio = {
+    enable = true;
+    package = pkgs.pulseaudioFull;
+    extraConfig = ''
+      load-module module-switch-on-connect
+      load-module module-bluetooth-discover a2dp_config="ldac_eqmid=hq ldac_fmt=f32"
+    '';
+    extraModules = [ pkgs.pulseaudio-modules-bt ];
+  };
+
+  # Enable bluetooth.
+  hardware.bluetooth = {
+    enable = true;
+    package = pkgs.bluezFull;
+    powerOnBoot = true;
+    config = {
+      General = {
+        Autoconnect = true;
+        ControllerMode = "bredr";
+        MultiProfile = "multiple";
+        Enable = "Source,Sink,Media,Socket";
+      };
+    };
+  };
+}
