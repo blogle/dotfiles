f6c0df1eaa2d5524415772d1e012b505c82454cf
Brandon Ogle <brandon@standardcognition.com>
2021-05-24 12:24:46 -0500

    Bumped nixpkgs, though it required downgrading the kernel 5.8->5.4

--- packages.nix
+++ packages.nix
@@ -1,8 +1,21 @@
 self: super:
 
+let
+in
+
 {
 
   cog = super.callPackage ./pkgs/cog.nix {};
-  linuxPackages = super.linuxPackages_5_8;
+  screenconfig = self.pythonPackages.screenconfig;
+  linuxPackages = super.linuxPackages_5_4;
+
+
+  python = self.python3;
+  pythonPackages = self.python.pkgs;
+  python3 = super.python3.override {
+    packageOverrides = self: super: {
+      screenconfig = super.callPackage ./pkgs/screenconfig.nix {};
+    };
+  };
 
 }
