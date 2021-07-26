c378db8cbe6f864284373b62f230eb7c7f5cbc54
Brandon Ogle <brandon@standard.ai>
2021-07-26 17:47:54 +0000

    Use the autogen hardware config for modulus

--- hosts/modulus/hardware-configuration.nix
+++ hosts/modulus/hardware-configuration.nix
@@ -4,31 +4,30 @@
 { config, lib, pkgs, modulesPath, ... }:
 
 {
-  imports = [ "${toString modulesPath}/installer/scan/not-detected.nix" ];
+  imports =
+    [ (modulesPath + "/installer/scan/not-detected.nix")
+    ];
 
-  boot.initrd.availableKernelModules = [ "xhci_pci" "nvme" "usb_storage" "sd_mod" ];
+  boot.initrd.availableKernelModules = [ "xhci_pci" "nvme" "usb_storage" "sd_mod" "rtsx_pci_sdmmc" ];
+  boot.initrd.kernelModules = [ "dm-snapshot" ];
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/85cdc85c-50f3-4b7b-bc5b-10fcc76a3c4c";
-      fsType = "ext4";
-    };
-
-  fileSystems."/home" =
-    { device = "/dev/disk/by-uuid/410b8f95-0a86-4eef-835e-fe8a5e0f9403";
+    { device = "/dev/disk/by-uuid/4d71dab8-edc2-4d39-8f3f-1108fa0383cf";
       fsType = "ext4";
     };
 
   fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/53DD-7C8F";
+    { device = "/dev/disk/by-uuid/03FE-1545";
       fsType = "vfat";
     };
 
   swapDevices =
-    [ { device = "/dev/disk/by-uuid/ca46fa77-579e-47d9-a98a-8df6a28bb075"; }
+    [ { device = "/dev/disk/by-uuid/b2608a35-429a-4798-a700-c2441bb28656"; }
     ];
 
-  nix.maxJobs = lib.mkDefault 12;
   powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
+  # high-resolution display
+  hardware.video.hidpi.enable = lib.mkDefault true;
 }
