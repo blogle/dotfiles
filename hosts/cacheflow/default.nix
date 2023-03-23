15ed29666466e07427075c06ffde87f3b3df9c24
Brandon Ogle <brandon@standardcognition.com>
2023-03-23 11:58:10 -0700

    Install google sdk and vim to cacheflow

--- hosts/cacheflow/default.nix
+++ hosts/cacheflow/default.nix
@@ -40,11 +40,15 @@
     home = "/home/ogle";
     uid = 1000;
     isNormalUser = true;
+    packages = with pkgs; [
+      vim
+      google-cloud-sdk
+    ];
 
     openssh.authorizedKeys.keys = [
       "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDMbxQQ6asa917aD8HTavEinmIEsm6G3pZEOv7Rf33JCvvrdCW5ktOsjBm0oeRLt3aeC0QZa3nrMXixP7GCmJQWFPnAsQLlrpZnNRte5GB9X0wcUTUcvLo1kXzTBB5CRhSwdVQ9+/Ztc+LSiObPqFfsYY2pa85wYU6Q+Hu+aYSDrTvCzcL1ojEvUKnOmSWFYQ+fmYV7skKJL3Xr66zpWeCKyVtY8h7Ju3H3IWZTTl8Fyqtej63uHxqjQlMNzEjUL9Nzmev+O8+lCKvHXG+8dQBAYe3+tsIi1NKLSODSKxLpka52XIiNrgGnnr74YTZ8sp8Sd9STr3HUPr7uNK5I8DSL brandon@standard.ai"
       ];
-  }; 
+  };
 
   users.mutableUsers = false;
 
