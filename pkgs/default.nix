4603c63c0468a252164b71e8bc1fafbf4fa81a45
Brandon Ogle <brandon@standardcognition.com>
2023-04-07 15:09:09 -0700

    Remove nvidia docker customization now that libnvidia-container supports cgroups v2

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -3,28 +3,7 @@ final: prev: {
     cbt = final.callPackage ./cbt.nix {};
     cog = final.callPackage ./cog.nix {};
 
-    nvidia-docker = final.mkNvidiaContainerPkg {
-      name = "nvidia-docker";
-      containerRuntimePath = "${final.docker}/libexec/docker/runc";
-      configTemplate = builtins.toFile "config.toml" ''
-		disable-require = false
-		#swarm-resource = "DOCKER_RESOURCE_GPU"
-
-		[nvidia-container-cli]
-		#root = "/run/nvidia/driver"
-		#path = "/usr/bin/nvidia-container-cli"
-		environment = []
-		debug = "/var/log/nvidia-container-runtime-hook.log"
-		ldcache = "/tmp/ld.so.cache"
-		load-kmods = true
-		no-cgroups = true
-		user = "root:root"
-		ldconfig = "@@glibcbin@/bin/ldconfig"
-      '';
-        additionalPaths = [ (final.callPackage "${final.path}/pkgs/applications/virtualization/nvidia-docker" { }) ];
-    };
-
-    obsidian = final.callPackage ./obsidian.nix {};
+    #obsidian = final.callPackage ./obsidian.nix {};
 
     st-clipboard = final.fetchurl {
         url = https://st.suckless.org/patches/clipboard/st-clipboard-0.8.3.diff;
